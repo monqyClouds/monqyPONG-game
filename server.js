aa5642e330e9ed2cb584037820151995dcfef74b
Monkey Clouds <marcellusarthurs@gmail.com>
2022-03-25 15:28:41 +0100

    Added socekt.io

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-const server = require("http").createServer();  
+const { createServer } = require("http");
+const path = require("path");
+
+const express = require("express");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	},
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"))
+})
+
+const PORT = 3000;
+
+io.on("connection", (socket) => {
+  console.log(`A user connected`);
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+});
